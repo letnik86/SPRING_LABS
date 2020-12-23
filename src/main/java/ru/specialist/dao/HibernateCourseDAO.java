7507f034e92c96bd0c64ae44125605eea12a8dad
Летов Никита Кириллович <letnik86@gmail.com>
2020-12-23 18:06:30 +0300

    Hibernate

---
+++ src/main/java/ru/specialist/dao/HibernateCourseDAO.java
@@ -0,0 +1,71 @@
+package ru.specialist.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+
+@Transactional
+@Repository("courseDao")
+public class HibernateCourseDAO implements CourseDAO {
+	private static final Log LOG = LogFactory.getLog(HibernateCourseDAO.class);
+
+
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Course findById(int id) {
+		return (Course)getSessionFactory().getCurrentSession().
+			byId(Course.class).load(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Course> findAll() {
+		return getSessionFactory().getCurrentSession().
+			createQuery("from Course c").list(); // HQL
+	}
+
+	@Override
+	public List<Course> findByName(String title) {
+		// 
+		return null;
+	}
+
+	@Override
+	public void insert(Course course) {
+		getSessionFactory().getCurrentSession().save(course);
+		LOG.info("Course saved with id: " + course.getId());
+	}
+
+	@Override
+	public void update(Course course) {
+		getSessionFactory().getCurrentSession().update(course);
+		LOG.info("Course updated with id: " + course.getId());
+	}
+
+	@Override
+	public void delete(int id) {
+		Course c = new Course();
+		c.setId(id);
+		getSessionFactory().getCurrentSession().delete(c);
+		LOG.info("Course deleted with id: " + c.getId());
+	}
+
+}
---
+++ src/main/java/ru/specialist/dbHibernate/App.java
@@ -0,0 +1,33 @@
+package ru.specialist.dbHibernate;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ru.specialist.dao.Course;
+import ru.specialist.dao.CourseDAO;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		CourseDAO courseDao = context.getBean(CourseDAO.class);
+		
+//		Course spring = new Course();
+//		spring.setTitle("Spring");
+//		spring.setLength(40);
+//		spring.setDescription("Spring framework");
+//		courseDao.insert(spring);
+		//courseDao.delete(8);
+//
+		for(Course c : courseDao.findAll())
+			System.out.println(c);
+		//Course c8 = courseDao.findById(8);
+		//System.out.println( c8 );
+		//c8.setLength(45);
+		//courseDao.update(c8);
+
+		context.close();    
+	}
+}
